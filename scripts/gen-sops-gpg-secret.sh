0a3415c4ba226e3edd0c73f6f8718d53afaba5e5
Jonathan Bartlett <jonathan@jonnobrow.co.uk>
2021-10-02 11:53:21 +0100

    Added sops configuration and scripts to generate keys

---
+++ scripts/gen-sops-gpg-secret.sh
@@ -0,0 +1,4 @@
+gpg --export-secret-keys --armor "${KEY_FP}" |
+kubectl create secret generic sops-gpg \
+--namespace=flux-system \
+--from-file=sops.asc=/dev/stdin
---
+++ scripts/gen-sops-key.sh
@@ -0,0 +1,15 @@
+export KEY_NAME="cappuccino.jonnobrow.co.uk"
+export KEY_COMMENT="flux secrets"
+
+gpg --batch --full-generate-key <<EOF
+%no-protection
+Key-Type: 1
+Key-Length: 4096
+Subkey-Type: 1
+Subkey-Length: 4096
+Expire-Date: 0
+Name-Comment: ${KEY_COMMENT}
+Name-Real: ${KEY_NAME}
+EOF
+
+gpg --list-secret-keys "${KEY_NAME}"
